e4c9f8a439be8a13af495aea67d23384166c0f7f
sumeet978 <searchsumeet@gmail.com>
2019-08-27 17:12:31 +0100

    re-routed homepage and add_space

--- app.rb
+++ app.rb
@@ -2,9 +2,26 @@ require 'sinatra'
 require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
-
+  enable :sessions
+  session[:list] = List.new
   get '/' do
+    @list_of_items = session[:list].show 
     erb :index
+  end
+
+  get '/add_space' do
+    erb :add_space
+  end
+
+  post '/add_space' do
+   name = params[:name]
+   description = params[:description]
+   price = params[:price]
+   email_address = params[:email_address]
+   phone_number = params[:phone_number]
+   item = ListItem.new(name, description, price, email_address, phone_number)
+   session[:list].add(item)
+   redirect "/"
   end
   
   run! if app_file == $0

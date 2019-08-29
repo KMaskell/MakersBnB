18df06e8820303eae0d9a34dee996e0d6852c054
sumeet978 <searchsumeet@gmail.com>
2019-08-29 15:19:29 +0100

    Fixed feature test

--- spec/feature/logout_spec.rb
+++ spec/feature/logout_spec.rb
@@ -1,12 +1,13 @@
 feature "logout" do
-  scenario " A logged in user can logout"
-  User.add(username: "Sumeet", password: "raina", email: sumeet@gmail.com)
+  scenario " A logged in user can logout" do
+  User.add("Sumeet", "raina", "sumeet@gmail.com")
   visit('/')
   fill_in('email', with: 'sumeet@gmail.com')
-  fill_in('password, with: 'sumeet')
+  fill_in('password', with: 'raina')
   click_button('log_in')
-  click_button('log_out')
+  click_on('Logout')
 
+  
   expect(page).to have_content "Goodbye!! See you again soon"
   end
 end
